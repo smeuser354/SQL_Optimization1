c1000a734543d2c16e2e1cca7f3bcad51da65f4f
smeuser354 <smeuser354@invisible.co>
2025-07-31 12:24:56 +0545

    Create table_schema.sql

---
+++ table_schema.sql
@@ -0,0 +1,38 @@
+CREATE TABLE  customers{
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    signup_date DATE,
+}
+
+CREATE TABLE products {
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    in_stock INT
+}
+
+CREATE TABLE orders {
+    id SERIAL PRIMARY KEY,
+    customer_id INT REFERENCES customers(id),
+    order_date DATE,
+    total DECIMAL(10, 2) NOT NULL,
+}
+
+CREATE TABLE order_items {
+    id SERIAL PRIMARY KEY,
+    order_id INT REFERENCES orders(id),
+    product_id INT REFERENCES products(id),
+    quantity INT NOT NULL,
+    unit_price DECIMAL(10, 2) NOT NULL,
+}
+
+CREATE TABLE reviews {
+    id SERIAL PRIMARY KEY,
+    customer_id INT REFERENCES customers(id),
+    product_id INT REFERENCES products(id),
+    rating INT CHECK (rating >= 1 AND rating <= 5),
+    remarks TEXT,
+    review_date DATE
+}
